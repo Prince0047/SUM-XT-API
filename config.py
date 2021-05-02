19c994f6376b84c5996b8bdf742bf8566fbb5de0
Prince0047 <sonubhadana838@gmail.com>
2021-05-02 11:29:11 +0530

    config

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-import click
-from flask.cli import with_appcontext
-from project import db, create_app
-from .models import User
 
-@click.command(name='create_table')
-@with_appcontext
-def create_database():
-    db.create_all(app=create_app)
+from project import db, create_app
+
+db.create_all(app=create_app)
